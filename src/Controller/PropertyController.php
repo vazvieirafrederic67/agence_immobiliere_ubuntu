7499c9d287e6526371528f1dcf5b43a3cc7e2aaa
frederic <vazvieirafrederic@outlook.fr>
2020-05-02 21:25:47 +0200

    mise a jour

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -21,6 +21,24 @@ class PropertyController extends AbstractController{
 public function index(): Response
 {
   
+    $property = new Property();
+    $property->setTitle('Mon premier bien');
+    $property->setPrice(200000);
+    $property->setRooms(4);
+    $property->setBedrooms(3);
+    $property->setDescription('Une petite description');
+    $property->setSurface(60);
+    $property->setFloor(4);
+    $property->setHeat(1);
+    $property->setCity('Montpellier');
+    $property->setAdress('15 boulevard Gambetta');
+    $property->setPostalCode(34000);
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($property);
+    //$em->flush();
+
+
+
 
     return $this->render('property/index.html.twig',['current_menu' => 'properties']);
 
